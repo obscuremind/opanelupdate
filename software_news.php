66c80812cc124901b4f6f89eed64b69becc98b9a
obscuremind <dreamer1@live.com.pt>
2024-09-24 09:11:50 +0100

    Add files via upload

---
+++ software_news.php
@@ -0,0 +1,6 @@
+</p>
+<h3 style="color:#534686";><b>Bem Vindos</h3></b>
+<br>Este esta é a versão inicial do painel</h2></br>
+<br>Disponivel para Atualizacao<b style="color:#FF5733";> 23/09/2024</b></h2></br>
+<br>Update 1 <b style="color:#FF5733";> Correçao de bugs em admin e rev</b></h2></br>
+
